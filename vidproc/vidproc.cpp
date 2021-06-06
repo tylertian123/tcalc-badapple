3f9a2a951e34bfb162bd16e5fa85e9364d827c25
Tyler Tian <tylertian123@gmail.com>
2021-06-06 02:48:58 -0400

    Add encoder code
    Complete encoder implementation and begin testing.

--- vidproc/vidproc.cpp
+++ vidproc/vidproc.cpp
@@ -1,7 +1,10 @@
-#include <string>
-#include <iostream>
-#include <fstream>
+#include <algorithm>
 #include <cstdint>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <limits>
+#include <string>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
@@ -16,7 +19,7 @@ constexpr unsigned int FRAME_INTERVAL = 1000 / FRAMERATE;
 // Util class for writing individual bits
 class BitStream {
     std::ostream &stream;
-    char buf;
+    uint8_t buf;
     int count;
 
 public:
@@ -113,7 +116,7 @@ const int RunLengthEncoder::OFFSETS[] = {
 /*
  * Perform the necessary resizing and conversion on a frame.
  */
-void process_frame(cv::Mat in, cv::Mat out) {
+void process_frame(cv::Mat &in, cv::Mat &out) {
     // Resize
     cv::Mat temp;
     int width = in.cols;
@@ -126,14 +129,18 @@ void process_frame(cv::Mat in, cv::Mat out) {
     cv::threshold(temp, out, 127, 255, cv::THRESH_BINARY_INV);
 }
 
+constexpr unsigned int CHUNK_COUNT_X = 8;
+constexpr unsigned int CHUNK_COUNT_Y = 8;
+
 /*
  * Compress & encode the video and write to the stream.
  *
- * This method divides the frame into 16 regions.
+ * This method divides the frame into regions.
  */
-void encode_video_16(cv::VideoCapture &cap, std::ostream &out) {
+void encode_video(cv::VideoCapture &cap, std::ostream &out, int frame_limit = std::numeric_limits<int>::max()) {
     cv::Mat frame;
     cv::Mat processed;
+    cv::Mat previous;
     // First frame is special
     cap.set(cv::CAP_PROP_POS_MSEC, 0);
     if (!cap.read(frame)) {
@@ -142,28 +149,91 @@ void encode_video_16(cv::VideoCapture &cap, std::ostream &out) {
     }
     process_frame(frame, processed);
     // Write frame size
-    uint8_t width = processed.cols;
-    uint8_t height = processed.rows;
-    out.put(width);
-    out.put(height);
+    const unsigned int fwidth = processed.cols;
+    const unsigned int fheight = processed.rows;
+    out.put(fwidth);
+    out.put(fheight);
+    // Find the chunk size
+    // Divide and round up; the right & bottom chunks are a little smaller
+    // Makes the code a little cleaner later on
+    const unsigned int CHUNK_WIDTH = (fwidth - 1) / CHUNK_COUNT_X + 1;
+    const unsigned int CHUNK_HEIGHT = (fheight - 1) / CHUNK_COUNT_Y + 1;
 
+    BitStream out_bits(out);
     // Run-length encode first frame
-    bool val;
-    unsigned int repeat = 0;
+    {
+        RunLengthEncoder encoder(out_bits);
+        for (auto it = processed.begin<uint8_t>(); it != processed.end<uint8_t>(); ++it) {
+            encoder << static_cast<bool>(*it);
+        }
+    }
 
+    // Keep track of previous frame to do frame diffs
+    previous = processed;
     for (unsigned int count = 1; ; count ++) {
+        // Hard stop for debugging purposes
+        if (count > frame_limit) {
+            std::cout << "Frame limit reached.\n";
+            return;
+        }
+
+        if (count % 50 == 0) {
+            std::cout << "Encoded " << (static_cast<double>(count) / FRAMERATE) << " seconds\n";
+        }
+
         cap.set(cv::CAP_PROP_POS_MSEC, count * FRAME_INTERVAL);
         if (!cap.read(frame)) {
             std::cout << "All frames read.\n";
             return;
         }
+        process_frame(frame, processed);
 
-        // cv::imshow("Processed frame", proc);
-        // int key = cv::waitKey();
-        // if (key == 'q') {
-        //     std::cout << "Quit\n";
-        //     return;
-        // }
+        // Find the chunks that changed
+        uint32_t mask = 1;
+        uint32_t changed_chunks = 0;
+        for (unsigned int cx = 0; cx < CHUNK_COUNT_X; cx ++) {
+            for (unsigned int cy = 0; cy < CHUNK_COUNT_Y; cy ++) {
+                for (unsigned int x = cx * CHUNK_WIDTH; x < std::min((cx + 1) * CHUNK_WIDTH, fwidth); x ++) {
+                    for (unsigned int y = cy * CHUNK_HEIGHT; y < std::min((cy + 1) * CHUNK_HEIGHT, fheight); y ++) {
+                        if (static_cast<bool>(processed.at<uint8_t>(y, x)) != static_cast<bool>(previous.at<uint8_t>(y, x))) {
+                            changed_chunks |= mask;
+                            goto next_chunk;
+                        }
+                    }
+                }
+next_chunk:
+                mask <<= 1;
+            }
+        }
+
+        // Write frame header
+        uint32_t header = changed_chunks;
+        for (unsigned int i = 0; i < CHUNK_COUNT_X * CHUNK_COUNT_Y; i ++) {
+            out_bits << (header & 1);
+            header >>= 1;
+        }
+
+        // Encode the frame, skipping unchanged chunks
+        RunLengthEncoder encoder(out_bits);
+        for (unsigned int x = 0; x < fheight; x ++) {
+            for (unsigned int y = 0; y < fheight; y ++) {
+                // Entered new chunk
+                if (y % CHUNK_HEIGHT == 0) {
+                    unsigned int cx = std::min(x / CHUNK_WIDTH, CHUNK_COUNT_X - 1);
+                    unsigned int cy = std::min(y / CHUNK_HEIGHT, CHUNK_COUNT_Y - 1);
+                    // Check that the chunk is changed
+                    if (!(changed_chunks & (1 << (cx * CHUNK_COUNT_Y + cy)))) {
+                        // Skip chunk if unchanged
+                        // Offset 1 for the loop
+                        y += CHUNK_HEIGHT - 1;
+                        continue;
+                    }
+                }
+                encoder << static_cast<bool>(processed.at<uint8_t>(y, x));
+            }
+        }
+
+        previous = processed;
     }
 }
 
@@ -175,6 +245,11 @@ int main(int argc, char **argv) {
     std::cout << "Using file " << argv[1] << "\n";
     std::string out_filename = argc > 2 ? argv[2] : "video.bin";
     std::cout << "Outputting to file " << out_filename << " (pass command line argument to override)\n";
+    int frame_limit = std::numeric_limits<int>::max();
+    if (argc > 3) {
+        frame_limit = std::atoi(argv[3]);
+        std::cout << "Only encoding the first " << frame_limit << " frames.\n";
+    }
 
     cv::VideoCapture cap;
     if (!cap.open(argv[1], cv::CAP_ANY)) {
@@ -189,5 +264,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    encode_video_16(cap, out_file);
+    encode_video(cap, out_file, frame_limit);
+    std::cout << "Done.\n";
 }
